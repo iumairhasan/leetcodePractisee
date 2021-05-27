45fb7cb00af9dbe3fbaa4013ed0fae7cd7689cde
iumairhasan <umairhasan1456@gmail.com>
2021-05-27 13:58:58 -0400

    601. Human Traffic of Stadium

--- Database/Leetcode.sql
+++ Database/Leetcode.sql
@@ -79,3 +79,68 @@ For example, according to the above table, we should output:
 select w.name, w.population, w.area
 from World w
 where w.area > 3000000 or w.population > 25000000 
+
+/*
+601. Human Traffic of Stadium
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| visit_date    | date    |
+| people        | int     |
++---------------+---------+
+visit_date is the primary key for this table.
+Each row of this table contains the visit date and visit id to the stadium with the number of people during the visit.
+No two rows will have the same visit_date, and as the id increases, the dates increase as well.
+ 
+
+Write an SQL query to display the records with three or more rows with consecutive id's, and the number of people is greater than or equal to 100 for each.
+
+Return the result table ordered by visit_date in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Stadium table:
++------+------------+-----------+
+| id   | visit_date | people    |
++------+------------+-----------+
+| 1    | 2017-01-01 | 10        |
+| 2    | 2017-01-02 | 109       |
+| 3    | 2017-01-03 | 150       |
+| 4    | 2017-01-04 | 99        |
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-09 | 188       |
++------+------------+-----------+
+
+Result table:
++------+------------+-----------+
+| id   | visit_date | people    |
++------+------------+-----------+
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-09 | 188       |
++------+------------+-----------+
+The four rows with ids 5, 6, 7, and 8 have consecutive ids and each of them has >= 100 people attended. Note that row 8 was included even though the visit_date was not the next day after row 7.
+The rows with ids 2 and 3 are not included because we need at least three consecutive ids.
+*/
+
+/* Write your T-SQL query statement below */
+(select s.id, s.visit_Date, s.people
+from Stadium s, Stadium a, Stadium b
+where (s.id - a.id = 1 and a.id - b.id = 1) and 
+    (s.people >= 100 and a.people >= 100 and b.people >= 100))
+union
+(select a.id, a.visit_Date, a.people
+from Stadium s, Stadium a, Stadium b
+where (s.id - a.id = 1 and a.id - b.id = 1) and 
+    (s.people >= 100 and a.people >= 100 and b.people >= 100))
+union
+(select b.id, b.visit_Date, b.people
+from Stadium s, Stadium a, Stadium b
+where (s.id - a.id = 1 and a.id - b.id = 1) and 
+    (s.people >= 100 and a.people >= 100 and b.people >= 100))
