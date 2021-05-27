cf4547280d02806a49649cca24be2ed0942d829d
iumairhasan <umairhasan1456@gmail.com>
2021-05-27 13:37:31 -0400

    595. Big Countries

--- Database/Leetcode.sql
+++ Database/Leetcode.sql
@@ -47,3 +47,35 @@ For example, after running your query, the above Person table should have the fo
 Delete t
 from Person t, Person p
 where t.Email = p.Email and t.Id > p.Id
+
+/*
+595. Big Countries
+There is a table World
+
++-----------------+------------+------------+--------------+---------------+
+| name            | continent  | area       | population   | gdp           |
++-----------------+------------+------------+--------------+---------------+
+| Afghanistan     | Asia       | 652230     | 25500100     | 20343000      |
+| Albania         | Europe     | 28748      | 2831741      | 12960000      |
+| Algeria         | Africa     | 2381741    | 37100000     | 188681000     |
+| Andorra         | Europe     | 468        | 78115        | 3712000       |
+| Angola          | Africa     | 1246700    | 20609294     | 100990000     |
++-----------------+------------+------------+--------------+---------------+
+A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
+
+Write a SQL solution to output big countries' name, population and area.
+
+For example, according to the above table, we should output:
+
++--------------+-------------+--------------+
+| name         | population  | area         |
++--------------+-------------+--------------+
+| Afghanistan  | 25500100    | 652230       |
+| Algeria      | 37100000    | 2381741      |
++--------------+-------------+--------------+
+*/
+
+/* Write your T-SQL query statement below */
+select w.name, w.population, w.area
+from World w
+where w.area > 3000000 or w.population > 25000000 
